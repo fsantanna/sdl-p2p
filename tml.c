08f3e3f6ae898ad229c8476bcf1b2fcd6c9a3510
fsantanna <francisco.santanna@gmail.com>
2022-07-21 17:37:57 -0300

    Revert "- merge tml/p2p"
    This reverts commit 15ed6fe71d07d01782e2bd89015f2e48efdfc8f5.

--- tml.c
+++ tml.c
@@ -1,7 +1,5 @@
 #include <SDL.h>
 #include <assert.h>
-
-#include "p2p.h"
 #include "tml.h"
 
 #define MAX_EVT 10000
@@ -9,10 +7,10 @@
 
 typedef struct {
     int     tick;
-    p2p_evt evt;
+    tml_evt evt;
 } tml_tick_evt;
 
-void tml_loop (int fps, int n, void* mem, void(*cb_sim)(p2p_evt), void(*cb_eff)(int), int(*cb_rec)(SDL_Event*,p2p_evt*), int(*cb_trv)(SDL_Event*,int,int,int*)) {
+void tml_loop (int fps, int n, void* mem, void(*cb_sim)(tml_evt), void(*cb_eff)(int), int(*cb_rec)(SDL_Event*,tml_evt*), int(*cb_trv)(SDL_Event*,int,int,int*)) {
     char MEM[MAX_MEM][n];
     int mpf = 1000 / fps;
     assert(1000%fps == 0);
@@ -29,7 +27,7 @@ void tml_loop (int fps, int n, void* mem, void(*cb_sim)(p2p_evt), void(*cb_eff)(
         tml_tick_evt buf[MAX_EVT];
     } Q = { 0, 0, {} };
 
-    cb_sim((p2p_evt) { TML_EVT_INIT, 0, {} });
+    cb_sim((tml_evt) { TML_EVT_INIT });
     memcpy(MEM[0], mem, n);
     //printf("<<< memcpy %d\n", 0);
 
@@ -37,15 +35,9 @@ _RET_REC_: {
 
     //printf("REC %d\n", S.tick);
     while (1) {
-        p2p_evt evt;
-        if (p2p_step(&evt)) {
-            cb_sim(evt);
-            cb_eff(0);
-#if 0
         if (Q.nxt < Q.tot) {
             cb_sim(Q.buf[Q.nxt++].evt);
             cb_eff(0);
-#endif
         } else {
             uint32_t now = SDL_GetTicks();
             if (now < S.nxt) {
@@ -55,7 +47,7 @@ _RET_REC_: {
             if (now >= S.nxt) {
                 S.tick++;
                 S.nxt += S.mpf;
-                cb_sim((p2p_evt) { TML_EVT_TICK, 1, {.i1=S.tick} });
+                cb_sim((tml_evt) { TML_EVT_TICK, {.tick=S.tick} });
                 if (S.tick % 100 == 0) {
                     assert(MAX_MEM > S.tick/100);
                     memcpy(MEM[S.tick/100], mem, n);    // save w/o events
@@ -64,7 +56,7 @@ _RET_REC_: {
                 cb_eff(0);
             } else {
                 SDL_Event sdl;
-                p2p_evt   evt;
+                tml_evt   evt;
                 assert(SDL_PollEvent(&sdl));
 
                 switch (cb_rec(&sdl, &evt)) {
@@ -72,16 +64,10 @@ _RET_REC_: {
                         break;
                     case TML_RET_QUIT:
                         return;
-#if 0
                     case TML_RET_REC:
                         assert(Q.tot < MAX_EVT);
                         Q.buf[Q.tot++] = (tml_tick_evt) { S.tick, evt };
                         break;
-#endif
-                    case TML_RET_REC: {
-                        p2p_bcast(S.tick, &evt);
-                        break;
-                    }
                     case TML_RET_TRV:
                         goto _RET_TRV_;
                 }
@@ -123,7 +109,7 @@ _RET_TRV_: {
 
             for (int i=fst; i<=new; i++) {
                 if (i > fst) { // first tick already loaded
-                    cb_sim((p2p_evt) { TML_EVT_TICK, 1, {.i1=i} });
+                    cb_sim((tml_evt) { TML_EVT_TICK, {.tick=i} });
                 }
                 while (e<Q.tot && Q.buf[e].tick==i) {
                     cb_sim(Q.buf[e].evt);
--- tml.h
+++ tml.h
@@ -14,12 +14,29 @@ enum {
     TML_EVT_NEXT
 };
 
+typedef struct {
+    uint8_t id;
+    union {
+        int tick;
+        int i1;
+        struct {
+            int _1,_2;
+        } i2;
+        struct {
+            int _1,_2,_3;
+        } i3;
+        struct {
+            int _1,_2,_3,_4;
+        } i4;
+    } pay;
+} tml_evt;
+
 void tml_loop (
     int fps,                                // desired frame rate
     int mem_n,                              // memory size in bytes
     void* mem_buf,                          // pointer to memory contents
-    void (*cb_sim) (p2p_evt),               // simulation callback
+    void (*cb_sim) (tml_evt),               // simulation callback
     void (*cb_eff) (int trv),               // effects callback
-    int (*cb_rec) (SDL_Event*,p2p_evt*),    // event recording callback
+    int (*cb_rec) (SDL_Event*,tml_evt*),    // event recording callback
     int (*cb_trv) (SDL_Event*,int,int,int*) // travel mode callback
 );
